65dd38dff862d7f10a4d689e22c138d601336a35
chenleicpp <chenleicpp@163.com>
2015-03-04 12:54:27 +0800

    修改手指按在屏幕上listView有抖动

--- src/me/maxwin/view/XListView.java
+++ src/me/maxwin/view/XListView.java
@@ -279,6 +279,9 @@ public class XListView extends ListView implements OnScrollListener {
 			break;
 		case MotionEvent.ACTION_MOVE:
 			final float deltaY = ev.getRawY() - mLastY;
+			if (Math.abs(deltaY) < 10) {
+				return super.onTouchEvent(ev);
+			}
 			mLastY = ev.getRawY();
 			if (getFirstVisiblePosition() == 0
 					&& (mHeaderView.getVisiableHeight() > 0 || deltaY > 0)) {
